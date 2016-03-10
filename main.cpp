1f76064cbb051756cd960f10db4b99b6a9b30417
darkskeeper <dartskeeper@mail.ru>
2016-03-10 16:53:00 +0300

    With output file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <QCoreApplication>
 #include <QFile>
+#include <QTextStream>
 #include <iostream>
 using namespace std;
 
@@ -8,10 +9,10 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
     int i;
     int *arr = new int [5];
-    QFile file("C:\\Users\\yuhoa\\Documents\\Qt\\ezpoker\\file.txt");
+    QFile file("C:\\Users\\yuhoa\\Documents\\Qt\\ezpoker\\input.txt");
     if (!file.open(QIODevice::Text | QIODevice::ReadOnly)) return 0;
     QByteArray ba = file.readLine();
-    file.close(); //close file
+    file.close();
     QList<QByteArray> baList = ba.split(' ');
     for (int i=0;i<baList.size();i++)
     {
@@ -27,17 +28,20 @@ int main(int argc, char *argv[])
             if(arr[i]!=sc[i1]) t1++;
         if(t1==t) sc[t++]=arr[i];
     }
+    QFile file1("C:\\Users\\yuhoa\\Documents\\Qt\\ezpoker\\output.txt");
+    if(!file1.open(QIODevice::Append)) return 0;
+    QTextStream out(&file1);
     switch (t)
     {
     case 1:
-        cout << "IMPOSSIBLE!";
+        out << "IMPOSSIBLE!";
         break;
     case 2:
         t1=0;
         for(i=0; i<5; i++)
             if(arr[i]==sc[0]) t1++;
-        if(t1==2||t1==3) cout<<"Full House";
-        else cout<<"Four of a Kind";
+        if(t1==2||t1==3) out<<"Full House";
+        else out<<"Four of a Kind";
         break;
     case 3:
         for(int i1=0; i1<t; i1++)
@@ -45,12 +49,12 @@ int main(int argc, char *argv[])
             t1=0;
             for(i=0; i<5; i++)
                 if(arr[i]==sc[i1]) t1++;
-            if(t1==2) {cout<<"Two Pairs";break;}
-            else if(t1==3) {cout<<"Three of a Kind";break;}
+            if(t1==2) {out<<"Two Pairs";break;}
+            else if(t1==3) {out<<"Three of a Kind";break;}
         }
         break;
     case 4:
-        cout<<"One Pair";
+        out<<"One Pair";
         break;
     case 5:
         int min1=arr[0];
@@ -65,10 +69,11 @@ int main(int argc, char *argv[])
             {
                 if(arr[i]==min1+1) t1=1;
             }
-            if(t1==0) {cout<<"Nothing"; break;}
+            if(t1==0) {out<<"Nothing"; break;}
         }
-        if(t1!=0) cout<<"Straight";
+        if(t1!=0) out<<"Straight";
         break;
     }
+    file1.close();
     return a.exec();
 }
