ca7888216a93802f47a85be460f14bbecc89b920
darkskeeper <dartskeeper@mail.ru>
2016-03-11 16:46:36 +0300

    Fixed C version

--- main_-_Copy.cpp
+++ main_-_Copy.cpp
@@ -24,15 +24,15 @@ int main()
         }
     }
     fclose(fr);
-    int sc[5];
-    sc[0] = arr[0];
-    int t=1, t1;
+    int unic_cards[5];
+    unic_cards[0] = arr[0];
+    int N_unic_cards=1, not_unic;
     for(i=1; i < 5; i++)
     {
-        t1=0;
-        for(int i1=0; i1<t; i1++)
-            if(arr[i]!=sc[i1]) t1++;
-        if(t1==t) sc[t++]=arr[i];
+        not_unic=0;
+        for(int i1=0; i1<N_unic_cards; i1++)
+            if(arr[i]!=unic_cards[i1]) not_unic++;
+        if(not_unic==N_unic_cards) unic_cards[N_unic_cards++]=arr[i];
     }
     if ((fw = fopen("OUTPUT.txt", "w")) == NULL)
     {
@@ -40,26 +40,27 @@ int main()
         system("pause");
         exit(1);
     }
-    switch (t)
+	int Nrepeats;
+    switch (N_unic_cards)
     {
     case 1:
         fputs("Impossible", fw);
         break;
     case 2:
-        t1=0;
+        Nrepeats=0;
         for(i=0; i<5; i++)
-            if(arr[i]==sc[0]) t1++;
-        if(t1==2||t1==3) fputs("Full House", fw);
+            if(arr[i]==unic_cards[0]) Nrepeats++;
+        if(Nrepeats==2||Nrepeats==3) fputs("Full House", fw);
         else fputs("Four of a Kind", fw);
         break;
     case 3:
-        for(int i1=0; i1<t; i1++)
+		for(int i1=0; i1<N_unic_cards; i1++)
         {
-            t1=0;
+            Nrepeats = 0;
             for(i=0; i<5; i++)
-                if(arr[i]==sc[i1]) t1++;
-            if(t1==2) {fputs("Two Pairs", fw);break;}
-            else if(t1==3) {fputs("Three of a Kind", fw);break;}
+                if(arr[i]==unic_cards[i1]) Nrepeats++;
+            if(Nrepeats==2) {fputs("Two Pairs", fw);break;}
+            else if(Nrepeats==3) {fputs("Three of a Kind", fw);break;}
         }
         break;
     case 4:
@@ -71,16 +72,17 @@ int main()
         {
             if(arr[i]<min) min=arr[i];
         }
+		bool is_more_1;
         for(i=1; i<5; i++)
         {
-            t1=0;
+            is_more_1=false;
             for(int i1=0; i1<5; i1++)
             {
-				if(arr[i1]==min+1) {t1=1; min=arr[i1]; break;}
+				if(arr[i1]==min+1) {is_more_1=true; min=arr[i1]; break;}
             }
-            if(t1==0) {fputs("Nothing", fw); break;}
+            if(is_more_1==false) {fputs("Nothing", fw); break;}
         }
-        if(t1!=0) fputs("Straight", fw);
+        if(is_more_1==true) fputs("Straight", fw);
         break;
     }
     fclose(fw);
